4bb71d7a1c29c7108cb6cbbf8d74c8a8ec7fb3e1
Jon Seaman <jonmseaman@live.com>
2026-09-18 19:05:30 -0400

    oo-fjku: fix S6 role assertion - police survives a fight with a pirate, not the pirate

--- run_s6.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-export PATH="/ucrt64/bin:$PATH"
-export MINGW_PREFIX=/ucrt64
-export OO_APP_DIR="C:/Users/jon/OoliteMigration/upstream/oolite/build/meson_test/oolite.app"
-cd /c/Users/jon/OoliteMigration/.worktrees/oo-fjku
-python3.exe -m pytest upstream/oolite/tests/component/ -k s6_ -x -q
