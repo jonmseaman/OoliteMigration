c9913843d074ae338606110f2f505070660e5505
Jon Seaman <jonmseaman@live.com>
2026-09-19 14:22:05 -0400

    bead oo-0kq: QuickJS-ng backend: Context/Value/Object/ClassDef with exotic-method mapping

--- upstream/oolite/.qjswork/dbg2.cpp
+++
@@ -1,7 +0,0 @@
-#include "ooscript/JSEngine.hpp"
-#include <cstdio>
-using namespace ooscript;
-int main(){
-  Value n = nullValue();
-  printf("nullbits=%llx isObjOrNull=%d isObject=%d\n", (unsigned long long)n.bits, isObjectOrNull(n), isObject(n));
-}
